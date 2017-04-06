cdba485c09f9f302c60d96c2856e03b65f61f6cb
hh2010 <hasan.haq@gmail.com>
2017-04-06 04:28:35 -0500

    Added ability to select multiple locations

--- README.md
+++ README.md
@@ -1,28 +1,52 @@
 ## Riffle
-The five star rating system is broken. Using the
-[Yelp Academic Dataset]()
-as an example, over 66% of the reviews are positive (4 or 5 stars). Furthermore,
+The five star rating system is **broken**. Looking at the 4.1 million reviews in the
+[Yelp Academic Dataset](https://www.yelp.com/dataset_challenge),
+**over 66% of the review are positive** (4 or 5 stars). Furthermore,
 these reviews are not written by professional critics. There is a lot of bias,
-inaccuracies and inconsistencies in reviews.
+inaccuracy and inconsistency in reviews.
 
-So as a smart consumer, what do you do when you have an important date coming
-up and need to choose between three hot restaurants that are all rated 4.5 stars?
-You [riffle]() through reviews and try to pick out key features that are important
-to you. There are at least a couple problems with that: you can only parse
+So as a **smart consumer**, what do you do when you have an important date coming
+up and need to choose between three trendy restaurants that are all rated 4.5 stars?
+You [riffle](https://www.google.com/search?hl=en&q=riffle+definition) through reviews and try to pick out key features that are important
+to you.
+
+There are at least a couple problems with that: you can only parse
 a handful of reviews out of potentially thousands, and you may not effectively
 pick out those features that determine quality reviews.
 
-Don't rely on 5 star ratings to make important decisions. Let **Riffle** help
-you.
+Don't rely on 5 star ratings to make important decisions. Let **Riffle** do
+the work for you.
 
 ## Usage
-Just go to the website [TBA]()
+Just go to the website [riffle.hasanhaq.com](http://riffle.hasanhaq.com)
+and pick the spots you want to Riffle.
 
-Lot of modules needed and private settings
-xgboost
-yelpapi
-flask with secret key
+Currently only the top 10,000 Las Vegas locations (by number of Yelp reviews) are supported. If there
+is interest then I will add more locations.
 
+I also have a **demo** coming soon to illustrate the process.
+
+## Methodology
+!['Riffle Methodology'](ppt/riffle_method.png)
+
+I used machine learning techniques to train my model on the "language"
+that people use for **favorable** and **unfavorable** reviews.
+
+Then, I go through all the reviews in the dataset and model them as positive
+or negative and spit out a percentage. No more 5 star craziness. Think
+[Rotten Tomatoes](http://www.rottentomatoes.com).
+
+Next, I penalize the weighting of each review based on subjectivity,
+polarity, and word count. I use [NLTK]() sentiment analysis during this
+process.
+
+Finally, I populated a [Postgresql](https://www.postgresql.org/) database, pushed it to the cloud, and built a
+[Flask](http://flask.pocoo.org/) web app to analyze the results.
+
+## Supplemental Materials
+[Jupyter Notebook](riffle.ipynb)
+
+[Presentation](ppt/riffle.pptx)
 
 ## Contact
 If you have any questions or comments on this project, you can find me below:
--- __init__.py
+++ __init__.py
@@ -23,13 +23,6 @@ sql.pull_names()
 bus_name_list = [x for x, y in sql.names]
 bus_id_list = [y for x, y in sql.names]
 
-# Set up a default dictionary
-def_dict = {'business_id': 'Business ID', 'name': 'Business Name',
-            'city': 'City', 'country': 'Country', 'old_rating': 0,
-            'new_rating': 0, 'rev_count': 0, 'count_5': 0, 'count_4': 0,
-            'count_3': 0, 'count_2': 0, 'count_1': 0, 'fav_count': 0,
-            'unfav_count': 0, 'avg_wts': 0, 'delta': 0}
-
 # Initialize session variables
 def session_init():
     try:
@@ -40,18 +33,19 @@ def session_init():
     except KeyError:
         session['vis'] = 0
     try:
-        if session['www_dict']:
+        if session['www_tups']:
             pass
         else:
-            session['www_dict'] = dict(def_dict)
+            session['www_tups'] = ()
     except KeyError:
-        session['www_dict'] = dict(def_dict)
+        session['www_tups'] = ()
 
 # Configure the Flask app
-# DEBUG = True
+DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = key
+
 
 # Set up the default route
 @app.route("/", methods=['GET', 'POST'])
@@ -59,126 +53,124 @@ def reviews(names=bus_name_list, ids=bus_id_list):
     """
     Load the Riffle page and populate the variables used on the site
     """
-    global def_dict
     session_init()
     form = Form(request.form)
 
+    print request.form.getlist("business")
+
     try:
-        bus_name = request.form.get("business")
-        bus_id = ids[names.index(bus_name)]
+        bus_list = request.form.getlist("business")
+        bus_ids = [ids[names.index(x)] for x in bus_list]
     except:
-        bus_name = 'Bellagio Hotel'
-        bus_id = ids[names.index(bus_name)]
+        bus_list = ['Bellagio Hotel']
+        bus_ids = [ids[names.index(bus_list[0])]]
 
     # Pull the list of Business Names and IDs
     try:
-        session['www_dict'] = bus_data(bus_id)
+        session['www_tups'] = bus_data(bus_ids)
+
     except:
-        print "There was an error!"
+        # print "There was an error!"
+        raise
         session['vis'] = 0
 
-    return render_template('reviews.html', form=form, data=session['www_dict'],
-                           bus_names=names, default_bus=bus_name,
+    print session['www_tups']
+
+    return render_template('reviews.html', form=form, data=session['www_tups'],
+                           bus_names=names, default_bus=bus_list,
                            vis=session['vis'])
 
-def bus_data(bus_id):
+def bus_data(bus_ids):
     """
     Populate business data from Riffle module and Yelp API
     Input: Business ID number
     Output: Dictionary
     Side-Effects: Updated SQL database
+
+    The tuples should look like this:
+    tups = ('bus_id', 'name', 'city', 'new_rating', 'old_rating', 'delta',
+            'rev_count')
     """
-    # global vis
-    global def_dict
+    tups = []
+    for i in bus_ids:
+        # Instantiate the SQL class for the business data we will be pulling
+        sql = riffle.Sql(i)
 
-    bus_dict = dict(def_dict)
-
-    # Instantiate the SQL class for the business data we will be pulling
-    sql = riffle.Sql(bus_id)
-
-    # Check if we have previously analyzed the requested business
-    # If not, pull the raw data and processing the data
-    if sql.check() is False:
-        pass
-    else:
-        sql_tup = sql.check()[0]
-
-        # If we render a form for a new business, then increase
-        #  the visibility variable to allow viewing of multiple
-        #  business ratings
-        if bus_dict['name'] == sql_tup[1]:
+        # Check if we have previously analyzed the requested business
+        # If not, pull the raw data and processing the data
+        if sql.check() is False:
             pass
         else:
-            session['vis'] += 1
+            sql_tup = sql.check()[0]
 
-        bus_dict['business_id'] = sql_tup[0]
-        bus_dict['name'] = sql_tup[1]
-        bus_dict['city'] = sql_tup[2]
-        bus_dict['country'] = sql_tup[3]
-        bus_dict['old_rating'] = sql_tup[4]
-        bus_dict['new_rating'] = sql_tup[5]
-        bus_dict['rev_count'] = sql_tup[6]
-        bus_dict['count_5'] = sql_tup[7]
-        bus_dict['count_4'] = sql_tup[8]
-        bus_dict['count_3'] = sql_tup[9]
-        bus_dict['count_2'] = sql_tup[10]
-        bus_dict['count_1'] = sql_tup[11]
-        bus_dict['fav_count'] = sql_tup[12]
-        bus_dict['unfav_count'] = sql_tup[12]
-        bus_dict['avg_wts'] = sql_tup[13]
-        bus_dict['delta'] = (bus_dict['new_rating'] - bus_dict['old_rating'])
-        return bus_dict
+            # If we render a form for a new business, then increase
+            #  the visibility variable to allow viewing of multiple
+            #  business ratings
+            if sql_tup[0] in bus_ids:
+                pass
+            else:
+                session['vis'] += 1
 
-    # Instantiate the XGBoost model
-    model = riffle.Model()
+            tups.append((sql_tup[0], sql_tup[1], sql_tup[2], sql_tup[5],
+                         sql_tup[4], sql_tup[5]-sql_tup[4], sql_tup[6]))
 
-    # Get business data (name, country, etc) from Yelp API
-    # Limited to 25,000 Yelp API calls per day
-    # There are over 4 million reviews and over 140,000 businesses in database
-    bus_info = riffle.Yelp(bus_id)
-
-    #  Grab review text from SQL database
-    sql.pull_reviews()
-
-    # Use our trained Random Forests model and TFIDF vectorizer
-    #  to determine whether each review is "Favorable" or "Unfavorable"
-    model.predict(sql.reviews)
-
-    # Conduct sentiment analysis and evaluate word counts in order to
-    #  "penalize" the weighting of reviews that don't fit the threshold
-    nlp = riffle.Nlp(sql.reviews, sql.stars, model.preds,
-                     bus_info.name, bus_info.country, bus_info.city)
-
-    # Assign variables from all the objects attributes we created
-    #  and then input them into a tuple.
-    # The tuple is used to populate the SQL database for faster lookup of
-    #  our analysis at a later time
-    # The tuple is also used to populate our dictionary which will be
-    #  used for variables that will be rendered on our website
-    name = nlp.name
-    city = nlp.city
-    country = nlp.country
-    old_rating = int(100 * nlp.stars_avg / 5)
-    new_rating = int(nlp.new_rating * 100)
-    rev_count = len(sql.reviews)
-    count_5 = sql.stars.count(5)
-    count_4 = sql.stars.count(4)
-    count_3 = sql.stars.count(3)
-    count_2 = sql.stars.count(2)
-    count_1 = sql.stars.count(1)
-    fav_count = (model.preds == 1).sum()
-    unfav_count = (model.preds == 0).sum()
-    avg_wts = nlp.avg_wts.sum() / len(avg_wts)
-    bus_tup = (bus_id, name, city, country, old_rating, new_rating,
-               rev_count, count_5, count_4, count_3, count_2, count_1,
-               fav_count, unfav_count, avg_wts)
-    sql.insert(bus_tup)
-    for key in bus_dict.keys()[:-1]:
-        x = 0
-        bus_dict[key] = bus_tup[x]
-        x += 1
-    session['vis'] += 1
-    return bus_dict
+    tups = sorted(tups, key=lambda x: x[3], reverse=True)
+    return tups
 
 if __name__ == "__main__":
     app.run()
+
+"""
+This is not necessary right now but will need to be updated at some point
+
+# Instantiate the XGBoost model
+model = riffle.Model()
+
+# Get business data (name, country, etc) from Yelp API
+# Limited to 25,000 Yelp API calls per day
+# There are over 4 million reviews and over 140,000 businesses in database
+bus_info = riffle.Yelp(bus_id)
+
+#  Grab review text from SQL database
+sql.pull_reviews()
+
+# Use our trained Random Forests model and TFIDF vectorizer
+#  to determine whether each review is "Favorable" or "Unfavorable"
+model.predict(sql.reviews)
+
+# Conduct sentiment analysis and evaluate word counts in order to
+#  "penalize" the weighting of reviews that don't fit the threshold
+nlp = riffle.Nlp(sql.reviews, sql.stars, model.preds,
+                 bus_info.name, bus_info.country, bus_info.city)
+
+# Assign variables from all the objects attributes we created
+#  and then input them into a tuple.
+# The tuple is used to populate the SQL database for faster lookup of
+#  our analysis at a later time
+# The tuple is also used to populate our dictionary which will be
+#  used for variables that will be rendered on our website
+name = nlp.name
+city = nlp.city
+country = nlp.country
+old_rating = int(100 * nlp.stars_avg / 5)
+new_rating = int(nlp.new_rating * 100)
+rev_count = len(sql.reviews)
+count_5 = sql.stars.count(5)
+count_4 = sql.stars.count(4)
+count_3 = sql.stars.count(3)
+count_2 = sql.stars.count(2)
+count_1 = sql.stars.count(1)
+fav_count = (model.preds == 1).sum()
+unfav_count = (model.preds == 0).sum()
+avg_wts = nlp.avg_wts.sum() / len(avg_wts)
+bus_tup = (bus_id, name, city, country, old_rating, new_rating,
+           rev_count, count_5, count_4, count_3, count_2, count_1,
+           fav_count, unfav_count, avg_wts)
+sql.insert(bus_tup)
+for key in bus_dict.keys()[:-1]:
+    x = 0
+    bus_dict[key] = bus_tup[x]
+    x += 1
+session['vis'] += 1
+return bus_dict
+"""
